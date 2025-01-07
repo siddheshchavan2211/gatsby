3ef9c295fde6f701139800560a802af986861044
Siddhesh Chavan <siddheshchavan02@gmail.com>
2025-01-07 14:31:47 +0000

    shadcn/ui added

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <Card className="w-72 mx-auto ">
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
