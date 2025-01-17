5a615750d4eb9e64876a0fec433bbc3f2ee22c22
gliese-backup <glieseb+1@proton.me>
2025-01-17 07:28:25 +0000

    add type Action

--- types.d.ts
+++ types.d.ts
@@ -4,3 +4,10 @@ interface Book {
   title: string;
   likes: number;
 }
+
+type Action =
+  | { type: "add"; payload: { author: string; title: string } }
+  | { type: "delete"; payload: { id: string } }
+  | { type: "like"; payload: { id: string } }
+  | { type: "dislike"; payload: { id: string } }
+  | { type: "sort" };
